6d55bc6a60e201a5d46ba4bc5a9ab49f1976817d
TiltSub <robertedwards@protonmail.com>
2020-04-29 14:14:30 +0100

    redesign and scripts

--- .dotfiles/polybar/.config/polybar/launch.sh
+++ .dotfiles/polybar/.config/polybar/launch.sh
@@ -8,6 +8,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar monitor1 &
+polybar monitor2 &
 
 echo "Polybar launched...#!/usr/bin/env bash
 
